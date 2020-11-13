99ef1c0409075cf24b3fed40fe33b44884651441
Matthew Mitchell <mattforsure@gmail.com>
2020-11-12 18:39:13 -0600

    gfsdjgxcmnxh,mvgxjcmz

---
+++ main.js
@@ -0,0 +1,7 @@
+var branding = document.getElementById("brandingLink");
+var class1 =  document.getElementById("class1");
+
+if (branding)
+    branding.addEventListener('click', function (){
+    class1.style.display = "none";
+})
